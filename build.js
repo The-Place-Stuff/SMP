0d3caca26a110210d6ce3c02e3ca5a3f2fdc1820
ExDrill <72329798+ExDrill@users.noreply.github.com>
2024-05-15 20:21:38 -0500

    Update build script

--- build.js
+++ build.js
@@ -1,51 +1,18 @@
 const fs = require('fs-extra')
+const createPack = require('./definitions')
 
-const dataPackDefinition = {
-    pack: {
-        description: 'Data for The Place SMP',
-        pack_format: 43
-    }
-}
-const resourcePackDefinition = {
-    pack: {
-        description: 'Assets for The Place SMP',
-        pack_format: 33
-    }
-}
-compile()
+build()
 
-function compile() {
+function build() {
     const settings = JSON.parse(fs.readFileSync('./settings.json'))
 
     if (typeof settings.dataOutput == 'undefined') {
         console.warn('Failed to find element dataOutput in settings.json')
     }
-    else compileDataPack(`${settings.dataOutput}/the_place_smp`)
+    else createPack(`${settings.dataOutput}/the_place_smp`, 'data')
 
     if (typeof settings.resourceOutput == 'undefined') {
         console.warn('Failed to find element resourceOutput in settings.json')
     }
-    else compileResourcePack(`${settings.resourceOutput}/the_place_smp`)
-}
-
-function compileDataPack(packFolder) {
-    if (!fs.existsSync(packFolder)) {
-        fs.mkdirSync(packFolder)
-    }
-    fs.writeFileSync(`${packFolder}/pack.mcmeta`, JSON.stringify(dataPackDefinition, null, 4))
-    fs.removeSync(`${packFolder}/data`)
-    fs.copySync('./data', `${packFolder}/data`, {
-        overwrite: true
-    })
-}
-
-function compileResourcePack(packFolder) {
-    if (!fs.existsSync(packFolder)) {
-        fs.mkdirSync(packFolder)
-    }
-    fs.writeFileSync(`${packFolder}/pack.mcmeta`, JSON.stringify(resourcePackDefinition, null, 4))
-    fs.removeSync(`${packFolder}/assets`)
-    fs.copySync('./assets', `${packFolder}/assets`, {
-        overwrite: true
-    })
+    else createPack(`${settings.resourceOutput}/the_place_smp`, 'assets')
 }
---
+++ definitions.js
@@ -0,0 +1,31 @@
+const fs = require('fs-extra')
+
+const dataPackDefinition = {
+    pack: {
+        description: 'Data for The Place SMP',
+        pack_format: 43
+    }
+}
+const resourcePackDefinition = {
+    pack: {
+        description: 'Assets for The Place SMP',
+        pack_format: 33
+    }
+}
+
+/**
+ * @param {string} output - The output directory of the pack.
+ * @param {'assets' | 'data'} type - The type of pack that should be compiled.
+ */
+function createPack(output, type) {
+    if (!fs.existsSync(output)) {
+        fs.mkdirSync(output)
+    }
+    fs.writeFileSync(`${output}/pack.mcmeta`, JSON.stringify(type == 'assets' ? resourcePackDefinition : dataPackDefinition, null, 4))
+    fs.removeSync(`${output}/${type}`)
+    fs.copySync('./data', `${output}/${type}`, {
+        overwrite: true
+    })
+}
+
+module.exports = createPack
